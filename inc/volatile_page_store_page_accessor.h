ad6c12411417f97022fc29141e2e78d61e937274
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-03 22:17:13 +0530

    need the obvious parameter

--- inc/volatile_page_store_page_accessor.h
+++ inc/volatile_page_store_page_accessor.h
@@ -3,12 +3,12 @@
 
 #include<volatile_page_store.h>
 
-void* get_new_page_for_vps(uint64_t* page_id);
+void* get_new_page_for_vps(volatile_page_store* vps, uint64_t* page_id);
 
-void* acquire_page_for_vps(uint64_t page_id);
+void* acquire_page_for_vps(volatile_page_store* vps, uint64_t page_id);
 
-void release_page_for_vs(void* page, int free_page);
+void release_page_for_vs(volatile_page_store* vps, void* page, int free_page);
 
-void free_page_for_vps(uint64_t page_id);
+void free_page_for_vps(volatile_page_store* vps, uint64_t page_id);
 
 #endif
--- src/volatile_page_store_page_accessor.c
+++ src/volatile_page_store_page_accessor.c
@@ -2,22 +2,22 @@
 
 #include<block_io_mmap_wrapper.h>
 
-void* get_new_page_for_vps(uint64_t* page_id)
+void* get_new_page_for_vps(volatile_page_store* vps, uint64_t* page_id)
 {
 	// TODO
 }
 
-void* acquire_page_for_vps(uint64_t page_id)
+void* acquire_page_for_vps(volatile_page_store* vps, uint64_t page_id)
 {
 	// TODO
 }
 
-void release_page_for_vs(void* page, int free_page)
+void release_page_for_vs(volatile_page_store* vps, void* page, int free_page)
 {
 	// TODO
 }
 
-void free_page_for_vps(uint64_t page_id)
+void free_page_for_vps(volatile_page_store* vps, uint64_t page_id)
 {
 	// TODO
 }
