e752e1c7abdadb98a43fe9a7577fefa1ba4b22b3
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-21 08:36:03 +0530

    added declarations for the mmaped_file_pool functions external api

---
+++ inc/mmaped_file_pool.h
@@ -0,0 +1,19 @@
+#ifndef MMAPED_FILE_POOL_H
+#define MMAPED_FILE_POOL_H
+
+#include<mmaped_file_pool_public.h>
+
+int initialize_mmaped_file_pool(mmaped_file_pool* mfp, pthread_mutex_t* external_lock, blockfile* file, uint64_t hashmaps_size);
+
+void* acquire_page(mmaped_file_pool* mfp, uint64_t page_id);
+
+int release_page(mmaped_file_pool* mfp, void* page);
+
+// get page_id for only an acquired page
+uint64_t get_page_id_for_frame(mmaped_file_pool* mfp, const void* frame);
+
+void discard_all_unreferenced_frame_descs(mmaped_file_pool* mfp);
+
+void deinitialize_mmaped_file_pool(mmaped_file_pool* mfp);
+
+#endif
