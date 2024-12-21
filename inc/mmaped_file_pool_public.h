e2ea14ed3c0e72ff7e83e2e4589eeb4b30e2a41c
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-21 08:31:23 +0530

    added structure for mmaped_file_pool

---
+++ inc/mmaped_file_pool_public.h
@@ -0,0 +1,29 @@
+#ifndef MMAPED_FILE_POOL_PUBLIC_H
+#define MMAPED_FILE_POOL_PUBLIC_H
+
+#include<pthread.h>
+#include<hashmap.h>
+#include<linkedlist.h>
+
+struct mmaped_file_pool
+{
+	blockfile* file;
+
+	int has_internal_lock : 1;
+	union
+	{
+		pthread_mutex_t* external_lock;
+		pthread_mutex_t  internal_lock;
+	};
+
+	// hashtable => page_id (uint64_t) -> frame descriptor
+	hashmap page_id_to_frame_desc;
+
+	// hashtable => frame (void*) -> frame descriptor
+	hashmap frame_ptr_to_frame_desc;
+
+	// linkedlist of all frame_descs that are unreferenced, i.e. reference_counter = 0
+	linkedlist unreferenced_frame_descs_lru_lists;
+};
+
+#endif
