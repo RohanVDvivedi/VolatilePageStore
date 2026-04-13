3016fb0de2642940f0623cc44a45aaeb39e0ef04
RohanVDvivedi <rohandvivedi@gmail.com>
2026-04-13 23:26:08 +0530

    added functions to insert and remove from the free list

--- src/volatile_page_store_page_accessor.c
+++ src/volatile_page_store_page_accessor.c
@@ -9,6 +9,59 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+typedef struct free_list_vps_node free_list_vps_node;
+struct free_list_vps_node
+{
+	uint64_t prev_page_id;
+	uint64_t next_page_id;
+};
+
+static void insert_in_free_pages_list(volatile_page_store* vps, uint64_t page_id, void* page)
+{
+	// initialize the node on the page
+	free_list_vps_node* page_node = page;
+	page_node->prev_page_id = vps->user_stats.NULL_PAGE_ID;
+	page_node->next_page_id = vps->user_stats.NULL_PAGE_ID;
+
+	// insert it at head
+	if(vps->free_pages_list_head_page_id != vps->user_stats.NULL_PAGE_ID) // if the head_page exists, make it's previous point to this new page
+	{
+		free_list_vps_node* head_page_node = acquire_page(&(vps->pool), vps->free_pages_list_head_page_id);
+		head_page_node->prev_page_id = page_id;
+		release_page(&(vps->pool), head_page_node);
+	}
+	page_node->next_page_id = vps->free_pages_list_head_page_id;
+
+	vps->free_pages_list_head_page_id = page_id;
+
+	release_page(&(vps->pool), page_node);
+}
+
+static void* remove_from_free_pages_list(volatile_page_store* vps, uint64_t page_id)
+{
+	free_list_vps_node* page_node = acquire_page(&(vps->pool), page_id);
+
+	if(page_node->prev_page_id != vps->user_stats.NULL_PAGE_ID) // if a prev_page exists, re position its next
+	{
+		free_list_vps_node* prev_page_node = acquire_page(&(vps->pool), page_node->prev_page_id);
+		prev_page_node->next_page_id = page_node->next_page_id;
+		release_page(&(vps->pool), prev_page_node);
+	}
+
+	if(page_node->next_page_id != vps->user_stats.NULL_PAGE_ID) // if a next_page exists, re position its next
+	{
+		free_list_vps_node* next_page_node = acquire_page(&(vps->pool), page_node->next_page_id);
+		prev_page_node->prev_page_id = page_node->prev_page_id;
+		release_page(&(vps->pool), next_page_node);
+	}
+
+	// redundant but we will initialize its next and prev page_ids
+	page_node->prev_page_id = vps->user_stats.NULL_PAGE_ID;
+	page_node->next_page_id = vps->user_stats.NULL_PAGE_ID;
+
+	return page_node;
+}
+
 static void* allocate_from_free_list(volatile_page_store* vps, uint64_t* page_id)
 {
 	// no pages in free list
