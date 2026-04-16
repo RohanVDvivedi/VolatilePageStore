72fd5a52b07ab0d4fcb9a1b66d6e68d4dd467e45
RohanVDvivedi <rohandvivedi@gmail.com>
2026-04-16 22:10:49 +0530

    fixed truncator

--- src/volatile_page_store_truncator.c
+++ src/volatile_page_store_truncator.c
@@ -17,41 +17,41 @@
 
 static void perform_truncation(volatile_page_store* vps)
 {
-	if(vps->active_page_count > 0)
+	// make trailing active_pages that are free, into inactive_pages
+	while(vps->active_page_count > 0)
 	{
-		uint64_t new_active_page_count = vps->active_page_count;
+		uint64_t page_id = (vps->active_page_count-1);
 
-		while(new_active_page_count > 0)
+		// we can directly truncate a trailing free space mapper page
+		if(is_free_space_mapper_page_vps(page_id, &(vps->stats)))
 		{
-			uint64_t page_id = new_active_page_count-1;
-
-			// we can directly truncate a trailing free space mapper page
-			if(is_free_space_mapper_page_vps(page_id, &(vps->stats)))
-			{
-				new_active_page_count--;
-				continue;
-			}
-
-			// else if it is free, then also we truncate it
-			if(is_free_page_vps(vps, page_id))
-			{
-				void* page = remove_from_free_pages_list_vps(vps, page_id);
-				release_page(&(vps->pool), page);
-				new_active_page_count--;
-				continue;
-			}
-
-			break;
+			vps->active_page_count--;
+			continue;
 		}
 
-		// truncate the file
+		// else if it is free, then also we truncate it
+		if(is_free_page_vps(vps, page_id))
 		{
+			void* page = remove_from_free_pages_list_vps(vps, page_id);
+			release_page(&(vps->pool), page);
+			vps->active_page_count--;
+			continue;
+		}
+
+		break;
+	}
+
+	// truncate the file
+	{
+		uint64_t new_total_page_count = UINT_ALIGN_UP(vps->active_page_count, MMAP_GROUP_SIZE);
+		if(vps->total_page_count > new_total_page_count)
+		{
+			vps->total_page_count = new_total_page_count;
+
 			// before you shorten the file make sure that all free pages (which for sure are unreferenced) are unmaped
 			discard_all_unreferenced_frame_descs(&(vps->pool));
 
-			uint64_t block_count_per_page = vps->stats.page_size / get_block_size_for_block_file(&(vps->temp_file));
-			vps->active_page_count = new_active_page_count;
-			if(!truncate_block_file(&(vps->temp_file), (block_count_per_page * new_active_page_count)))
+			if(!truncate_block_file(&(vps->temp_file), vps->total_page_count * (vps->stats.page_size / get_block_size_for_block_file(&(vps->temp_file)))))
 			{
 				printf("ISSUEv :: unable to truncate temp file\n");
 				exit(-1);
